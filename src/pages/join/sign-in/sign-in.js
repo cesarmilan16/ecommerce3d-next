2df06d8dfbacdc19f6a98609fb755e46f7823d90
cesarmilan16 <cesar3bfermi@gmail.com>
2025-05-16 21:34:57 +0200

    Refactor code structure for improved readability and maintainability

--- src/pages/join/sign-in/sign-in.js
+++ src/pages/join/sign-in/sign-in.js
@@ -1,11 +1,16 @@
+import { JoinLayout } from "@/layouts";
 import { Heading } from "@chakra-ui/react";
 import styles from "./sign-in.module.scss";
 
 export default function SignInPage() {
   return (
-    <div className={styles.signIn}>
-        <Heading as="h3" size={"xl"}>SignInPage</Heading>
-    </div>
+    <>
+      <JoinLayout>
+        <div className={styles.signIn}>
+          <Heading as="h3" size={"lg"}>Iniciar sesión</Heading>
+        </div>
+      </JoinLayout>
+    </>
   );
 }
 
--- src/pages/join/sign-up/sign-up.js
+++ src/pages/join/sign-up/sign-up.js
@@ -1,10 +1,15 @@
+import { JoinLayout } from "@/layouts";
 import styles from "./sign-up.module.scss";
 import { Heading } from "@chakra-ui/react";
 
 export default function SignUp() {
   return (
-    <div>
-        <Heading as="h3" size={"xl"}>SignUpPage</Heading>
-    </div>
+    <>
+    <JoinLayout>
+      <div className={styles.signUp}>
+        <Heading as="h3" size={"lg"}>Crear cuenta</Heading>
+      </div>
+    </JoinLayout>
+    </>
   )
 }
