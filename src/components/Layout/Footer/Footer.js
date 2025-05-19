f8a544b0f6ce5480fd592d3e7afeabdf17627648
cesarmilan16 <cesar3bfermi@gmail.com>
2025-05-19 05:07:18 +0200

    feat: add Footer component with styles, integrate into BasicLayout, and enhance Menu with search functionality

---
+++ src/components/Layout/Footer/Footer.js
@@ -0,0 +1,72 @@
+import Link from 'next/link';
+import { Circle, Container, Icon } from '@chakra-ui/react';
+import { FaInstagram, FaTiktok, FaTwitter } from "react-icons/fa";
+import styles from './Footer.module.scss';
+
+export function Footer() {
+    return (
+        <div className={styles.footer}>
+            <Container maxW="6xl" px={4}>
+                <div className={styles.columns}>
+                    <div>
+                        <Link href="/" >
+                            <img src="/images/logo.png" alt="3D Hispalis" />
+                        </Link>
+                    </div>
+
+
+                    <div>
+                        <ul>
+                            <Link href={"#"}>Terminos y condiciones</Link>
+                            <Link href={"#"}>Políticas de privacidad</Link>
+                            <Link href={"#"}>Contacto</Link>
+                            <Link href={"#"}>FAQs</Link>
+                        </ul>
+                    </div>
+
+                    <div className={styles.social}>
+                        <Link href="#" isExternal>
+                            <Circle
+                                size="30px"
+                                bg="#E1306C"
+                                color="white"
+                                transition="all 0.2s ease"
+                                _hover={{ bg: "#EC4F89" }}
+                            >
+                                <Icon as={FaInstagram} boxSize={5} />
+                            </Circle>
+                        </Link>
+
+                        <Link href="#" isExternal>
+                            <Circle
+                                size="30px"
+                                bg="#000000"
+                                color="white"
+                                transition="all 0.2s ease"
+                                _hover={{ bg: "#1a1a1a" }}
+                            >
+                                <Icon as={FaTiktok} boxSize={4} />
+                            </Circle>
+                        </Link>
+
+                        <Link href="#" isExternal>
+                            <Circle
+                                size="30px"
+                                bg="#1DA1F2"
+                                color="white"
+                                transition="all 0.2s ease"
+                                _hover={{ bg: "#38B3F8" }}
+                            >
+                                <Icon as={FaTwitter} boxSize={4} />
+                            </Circle>
+                        </Link>
+                    </div>
+                </div>
+
+                <div className={styles.copyright}>
+                    <span>© 2025 3D Hispalis. Casi todos los derechos reservados.</span>
+                </div>
+            </Container>
+        </div>
+    )
+}
--- src/components/Layout/Menu/Menu.js
+++ src/components/Layout/Menu/Menu.js
@@ -1,16 +1,20 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
+import { Icon, Input } from "@chakra-ui/react";
+import { FaSearch, FaTimes } from "react-icons/fa";
 import { map } from "lodash";
 import { Category } from "@/api";
 import styles from './Menu.module.scss';
+import classNames from "classnames";
 
 const categoryCtrl = new Category();
 
 export function Menu(props) {
     const { isOpenSearch } = props;
     const [categories, setCategories] = useState(null);
-    console.log(categories);
-    
+    const [showSearch, setShowSearch] = useState(false);
+
+    const openCloseSearch = () => setShowSearch(prevState => !prevState);
 
     useEffect(() => {
       (async () => {
@@ -32,6 +36,24 @@ export function Menu(props) {
                 {category.title}
             </Link>
         ))}
+
+        <button className={styles.search} onClick={openCloseSearch}>
+            <Icon as={FaSearch} />
+        </button>
+
+        <div className={classNames(styles.inputContainer, {[styles.active]: showSearch})}>
+            <Input 
+            id="search-products" 
+            placeholder="Buscador" 
+            className={styles.input} 
+            _focus={true} 
+            />
+            <Icon 
+            as={FaTimes} 
+            className={styles.closeInput} 
+            onClick={openCloseSearch} 
+            />
+        </div>
     </div>
   )
 }
