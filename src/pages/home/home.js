46227da339a6303c58317071651e2dced7ab8ff4
cesarmilan16 <cesar3bfermi@gmail.com>
2025-06-09 18:50:17 +0200

    feat: Add categoryId object and integrate LatestProducts component for 'Figuras' in HomePage

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,7 +1,14 @@
-import { BasicLayout } from "@/layouts"
-import { Home } from "@/components/Home"
-import { Box, Container } from "@chakra-ui/react"
-import { BarTrust } from "@/components/Shared"
+import { BasicLayout } from "@/layouts";
+import { Home } from "@/components/Home";
+import { Box, Container } from "@chakra-ui/react";
+import { BarTrust } from "@/components/Shared";
+
+const categoryId = {
+  figuras: 2,
+  decoracion: 4,
+  accesorios: 6,
+  juegosDeMesa: 8,
+}
 
 export default function HomePage() {
   return (
@@ -18,6 +25,12 @@ export default function HomePage() {
         <Box h={100} />
 
         <BarTrust />
+
+        <Box h={100} />
+
+        <Container maxW={"container.lg"}>
+          <Home.LatestProducts title="Figuras" limit={3} categoryId={categoryId.figuras} />
+        </Container>
 
         <Box h={100} />
       </BasicLayout>
